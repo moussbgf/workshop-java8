191f5d6bfa4df452d3aa6a551beb30280342b45c
moussbgf DTA <mboughazif@gmail.com>
2017-04-12 17:59:58 +0200

    exo 1 (bis)

--- src/test/java/java8/ex01/Stream_01_Test.java
+++ src/test/java/java8/ex01/Stream_01_Test.java
@@ -131,7 +131,8 @@ public class Stream_01_Test {
 		// TODO Trouver la pizza la moins chère dont le prix est >= 950
 		Optional<Pizza> result = pizzas.stream()
 				.filter(p -> p.getPrice() >= 950)
-				.min((p1,p2) -> p1.getPrice().compareTo(p2.getPrice()))
+				//.min((p1,p2) -> p1.getPrice().compareTo(p2.getPrice()))
+				.min(Comparator.comparing(Pizza::getPrice))
 				;
 
 		assertThat(result.isPresent(), is(true));
